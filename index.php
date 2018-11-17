1f51f86d6ba28bf24b3ee2571ae0b4e9eba2b032
Stan Shih <stan.shih@iisigroup.com>
2018-11-17 13:48:53 +0800

    index改副檔名為php, 移植style到專屬檔案

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>CSS Template</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+
+<header>
+    <table id="headerTable">
+        <tr>
+            <th id="headerFront"></th>
+            <th id="headerTitle"><h2>Cities</h2></th>
+            <th id="headerLogin"></th>
+        </tr>
+    </table>
+</header>
+
+<section>
+  <nav>
+   
+  </nav>
+  
+  <article>
+    <h1>London</h1>
+    <p>London is the capital city of England. It is the most populous city in the  United Kingdom, with a metropolitan area of over 13 million inhabitants.</p>
+    <p>Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.</p>
+  </article>
+</section>
+
+<footer>
+  <p>Footer</p>
+</footer>
+
+</body>
+</html>
